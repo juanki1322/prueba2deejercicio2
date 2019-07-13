6702e05f0adc78172e66e0480ff94eb56cdf25ba
Juan Camilo Collazo <juancamilocollazo@Macbooks-MacBook-Pro-2.local>
2019-07-12 21:34:20 -0500

    Initial Commit

---
+++ prueba2deejercicio2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  prueba2deejercicio2
+//
+//  Created by Juan Camilo Collazo on 7/12/19.
+//  Copyright © 2019 Juan Camilo Collazo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
